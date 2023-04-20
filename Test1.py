61dee14258fa1c7f4becb1128810a5d2ebe3ac7f
Manda <atchyuta.bharadwaj.manda@intel.com>
2023-04-20 11:48:41 -0700

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -357,9 +357,18 @@ with left_column4:
                     shadow=True, startangle=90)
             ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.       
             st.pyplot(fig2)     
-    if chosenprodstages == "3" and t1a >0 and t20 >0 and t30 >0 and u2<1 and u3<1:        
+    if chosenprodstages == "3" and t1a >0 and t20 >0 and t30 >0 and u2<1 and u3<1:  
         with middle_column4:
             st.write('At Machine 2:')
+            sizes = [t20, t2s/N2s, CT2q]
+            explode = (0.1, 0, 0)  # only "explode" the 1st slice            
+            fig2, ax2 = plt.subplots()
+            ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                    shadow=True, startangle=90)
+            ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.       
+            st.pyplot(fig2) 
+        with right_column4:
+            st.write('At Machine 3:')
             sizes = [t30, t3s/N3s, CT3q]
             explode = (0.1, 0, 0)  # only "explode" the 1st slice            
             fig3, ax3 = plt.subplots()
@@ -368,16 +377,28 @@ with left_column4:
             ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.       
             st.pyplot(fig3)   
 
+if chosenprodstages == "2" and t1a >0 and t20 >0 and u2 <1:
+        st.write('Complete System Processing time breakdown:')
+        sizes = [(t10+t20),((t1s/N1s)+(t2s/N2s)), (CT1q+CT2q)]
+        explode = (0.1, 0, 0)  # only "explode" the 1st slice            
+        fig4, ax4 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.       
+        st.pyplot(fig4)  
 
 
-st.write('Complete System Processing time breakdown:')
-sizes = [(t10+t20+t30),((t1s/N1s)+(t2s/N2s)+(t3s/N3s)), (CT1q+CT2q+CT3q)]
-explode = (0.1, 0, 0)  # only "explode" the 1st slice            
-fig4, ax4 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-        shadow=True, startangle=90)
-ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.       
-st.pyplot(fig4)  
+if chosenprodstages == "3" and t1a >0 and t20 >0 and t30 >0 and u2<1 and u3<1:
+        st.write('Complete System Processing time breakdown:')
+        sizes = [(t10+t20+t30),((t1s/N1s)+(t2s/N2s)+(t3s/N3s)), (CT1q+CT2q+CT3q)]
+        explode = (0.1, 0, 0)  # only "explode" the 1st slice            
+        fig4, ax4 = plt.subplots()
+        ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+                shadow=True, startangle=90)
+        ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.       
+        st.pyplot(fig4)  
+
+
 
 
 
